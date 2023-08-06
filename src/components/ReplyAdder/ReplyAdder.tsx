8cc44ff3ac47f858f65bc1b29fbd276d5d630de9
PatrickLee22 <patmlee22@gmail.com>
2023-08-06 13:17:46 -0500

    initial commit

---
+++ src/components/ReplyAdder/ReplyAdder.tsx
@@ -0,0 +1,80 @@
+import './ReplyAdder.css'
+
+import { useState } from 'react'
+import { User, ReplyData } from '../../models'
+
+interface Props{
+  username: string,
+  image: {
+    png: string,
+    webp: string,
+  }
+  replyingTo: string,
+  addReplyRequest: (newReply: ReplyData) => void,
+}
+
+interface State {
+  currUser: User,
+  replyingTo: string,
+  text: string,
+}
+
+
+const ReplyAdder = ({username, image, replyingTo, addReplyRequest}: Props) =>{
+
+  const [context, setContext] = useState<State>({
+    currUser: {
+      username: username,
+      image: image,
+    },
+    replyingTo: replyingTo,
+    text: ``,
+  })
+
+  const sendReply = () =>{
+    const newReply: ReplyData ={
+      id: Math.floor(Math.random() * 100) + 5,
+      content: context.text,
+      createdAt: 'Just Now',
+      score: 0,
+      replyingTo: replyingTo,
+      user: context.currUser,
+    }
+
+    addReplyRequest(newReply)
+    setContext({...context, text: ''})
+  }
+
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setContext({...context, text: e.target.value.slice(e.target.value.indexOf(',') + 1)})
+  }
+
+
+
+  return <section className='replyadder-section'>
+    <img 
+      src={'src/assets/' + context.currUser.image.png.substring(2)} 
+      className='adder-profile'
+      alt='juliusomo profile pic'
+    />
+    <textarea
+      className='replyadder-textbox'
+      placeholder='Add a reply...'
+      
+      onChange={(e) => handleChange(e)}
+    >
+      {`@${context.replyingTo}, ${context.text}`}
+    </textarea>
+    <button
+      className='replyadder-send'
+      onClick={() => sendReply()}
+    >
+      REPLY
+    </button>
+  </section>
+
+}
+
+
+export default ReplyAdder
